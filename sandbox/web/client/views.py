4ebf1723e34c40208e837d112c11330470f055fb
Luke Graybill <killarny@gmail.com>
2013-04-28 17:02:34 +0000

    Require login to view chat.

--- sandbox/web/client/views.py
+++ sandbox/web/client/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 def login(request):
     return render(request, 'client/login.html')
 
+@login_required
 def chat(request):
     context = {
         'session_id': request.session.session_key,
