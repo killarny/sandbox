5c06ad5cb6e183149a15c460c8ee371709ac1bec
Luke Graybill <killarny@gmail.com>
2013-04-28 17:01:38 +0000

    Added login view.

--- sandbox/web/client/urls.py
+++ sandbox/web/client/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('sandbox.web.client.views',
     url(r'^$', 'chat', name='chat'),
+    url(r'^login/$', 'login', name='login'),
 )
--- sandbox/web/client/views.py
+++ sandbox/web/client/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
+def login(request):
+    return render(request, 'client/login.html')
+
 def chat(request):
     context = {
         'session_id': request.session.session_key,
